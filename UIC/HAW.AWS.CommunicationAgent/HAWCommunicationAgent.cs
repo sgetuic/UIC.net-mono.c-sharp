5f2f31952cbc55205342a8138dea545f4bcd9a5a
DESKTOP-ND8TAHU\Niksch <wastl.n@gmx.de>
2018-04-27 18:46:53 +0200

    Commit with comments

---
+++ UIC/HAW.AWS.CommunicationAgent/HAWCommunicationAgent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UIC.Communication.M2mgo.CommunicationAgent.Configuration;
+using UIC.Communication.M2mgo.CommunicationAgent.Mqtt;
+using UIC.Communication.M2mgo.CommunicationAgent.Mqtt.messaging;
+using UIC.Communication.M2mgo.CommunicationAgent.Mqtt.messaging.Payload;
+using UIC.Communication.M2mgo.CommunicationAgent.Mqtt.messaging.Topic;
+using UIC.Communication.M2mgo.CommunicationAgent.Translation.DeviceManagement;
+using UIC.Communication.M2mgo.CommunicationAgent.Translation.Project;
+using UIC.Communication.M2mgo.CommunicationAgent.WebApi;
+using UIC.Communication.M2mgo.CommunicationAgent.WebApi.infrastructure;
+using UIC.Framework.Interfaces.Edm;
+using UIC.Framework.Interfaces.Edm.Value;
+using UIC.Framework.Interfaces.Project;
+using UIC.Util.Logging;
+using UIC.Util.Serialization;
+
+
+
+namespace HAW.AWS.CommunicationAgent 
+{
+    class HAWCommunicationAgent : UIC.Framework.Interfaces.Communication.Application.CommunicationAgent
+    {
+        public void Connect(Action<Command> commandHandler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(string debug)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(string serialId, UicProject project, List<EmbeddedDriverModule> edms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(DatapointValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(IEnumerable<DatapointValue> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(AttributeValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(IEnumerable<AttributeValue> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UIC/HAW.AWS.CommunicationAgent/RESTClient/RESTClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HAW.AWS.CommunicationAgent.RESTClient
+{
+    static class RESTClient
+    {
+        static HttpClient client = new HttpClient();
+
+       async public static void Push(String msg)
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:8080/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.PutAsJsonAsync($"rest/iot/, "+msg);
+            response.EnsureSuccessStatusCode();
+
+        }
+    }
+}
