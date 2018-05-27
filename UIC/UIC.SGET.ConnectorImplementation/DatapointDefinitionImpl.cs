97b32d985c5c56042f09020cb2881cef5ebc7936
Daniel <s-dkrueg@haw-landshut.de>
2018-05-27 17:16:07 +0200

    fixed local project file loading

---
+++ UIC/UIC.SGET.ConnectorImplementation/DatapointDefinitionImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UIC.Framework.Interfaces.Project;
+using UIC.Framework.Interfaces.Edm.Definition;
+using UIC.Framework.Interfaces.Util;
+
+namespace UIC.SGET.ConnectorImplementation
+{
+    public class DatapointDefinitionImpl : DatapointDefinition
+    {
+        public string ProjectKey {  get; set; }
+
+        public Guid Id { get; set; }
+
+        public string Label { get; set; }
+
+        public string Description { get; set; }
+
+        public UicDataType DataType { get; set; }
+
+        public string Uri { get; set; }
+    }
+}
